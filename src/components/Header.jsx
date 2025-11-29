652b7996a8158179f1ec031ffc980070c18b949e
Ahmed Abd El-Latif <46932157+AhmedAbdoDev@users.noreply.github.com>
2025-11-29 22:10:56 +0200

    Fixing Admin Testing team problems 'header -> search'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -90,21 +90,6 @@ function Header() {
                 Clinics
               </Link>
             </div>
-
-            <div className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="22"
-                height="22"
-                viewBox="0 0 24 24"
-                className="text-[#1CBCCF]"
-              >
-                <path
-                  fill="currentColor"
-                  d="M9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l5.6 5.6q.275.275.275.7t-.275.7t-.7.275t-.7-.275l-5.6-5.6q-.75.6-1.725.95T9.5 16m0-2q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"
-                />
-              </svg>
-            </div>
           </div>
         </div>
       </div>
@@ -140,8 +125,8 @@ function Header() {
               to="/"
               onClick={() => setMenuOpen(false)}
               className={`py-3 px-4 text-lg font-medium transition duration-300 ${
-                isActive("/") 
-                  ? "text-[#1CBCCF] bg-blue-50 rounded-lg" 
+                isActive("/")
+                  ? "text-[#1CBCCF] bg-blue-50 rounded-lg"
                   : "text-gray-700 hover:text-[#1CBCCF] hover:bg-gray-50 rounded-lg"
               }`}
             >
@@ -151,8 +136,8 @@ function Header() {
               to="/clinics"
               onClick={() => setMenuOpen(false)}
               className={`py-3 px-4 text-lg font-medium transition duration-300 ${
-                isActive("/clinics") 
-                  ? "text-[#1CBCCF] bg-blue-50 rounded-lg" 
+                isActive("/clinics")
+                  ? "text-[#1CBCCF] bg-blue-50 rounded-lg"
                   : "text-gray-700 hover:text-[#1CBCCF] hover:bg-gray-50 rounded-lg"
               }`}
             >
